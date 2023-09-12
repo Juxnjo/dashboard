98ab0239aefc5515441b7c6875e596da8eea2632
Juxnjo <ramirezjuanj0117@gmail.com>
2023-09-12 10:52:18 -0500

    viewport structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-export default function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { ProfilePage } from "./pages/ProfilePage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { NavbarComponent } from './components/Navbar/NavbarComponent';
+
+
+export default function App ()
+{
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello development!
-    </h1>
+    <BrowserRouter>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/register" element={ <RegisterPage /> } />
+        <Route path="/profile" element={ <ProfilePage /> } />
+        <Route path="/dashboard" element={ <DashboardPage /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/components/Navbar/NavbarComponent.jsx
@@ -0,0 +1,7 @@
+export const NavbarComponent = () => {
+  return (
+    <div className="bg-slate-300 w-full ">
+        <div className="h-24 p-6">Navbar</div>
+    </div>
+  )
+}
