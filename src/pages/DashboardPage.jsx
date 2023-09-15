d6395c96e2d583d092b8fd790ebcacf0f4a029bb
“Juan <ramirezjuanj0117@gmail.com>
2023-09-15 01:55:15 -0500

    dashboard meh

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -6,15 +6,15 @@ import { CardLg2Component } from "../components/Card/CardLg2Component";
 export const DashboardPage = () =>
 {
   return (
-    <div className="flex  ">
+    <div className="flex ">
       <SidebarComponent />
       <div className="bg-slate-200 w-full rounded-2xl p-10">
-        <div className="flex space-x-10  ">
+        <div className="flex  ">
           <CardLgComponent />
           <CardLg2Component />
           
         </div>
-        <div className="mt-10">
+        <div className="">
         <CardXlComponent/>
         </div>
 
