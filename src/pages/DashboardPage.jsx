79f55e46c23418100b168bfcc5d13578058271e9
“Juan <ramirezjuanj0117@gmail.com>
2023-09-14 21:52:51 -0500

    doing cards of dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,10 +2,10 @@ import { SidebarComponent } from "../components/Sidebar/SidebarComponent";
 
 export const DashboardPage = () => {
   return (
-    <div className="flex space-x-0 min-h-screen">
+    <div className="flex space-x-0 min-h-screen ">
       <SidebarComponent />
-      <div className="bg-slate-500 w-4/5 ">
-        <div className="p-6">
+      <div className="bg-slate-200 w-4/5 rounded-2xl">
+        <div className="p-10">
         <h1>Dashboard</h1>
         </div>
         
